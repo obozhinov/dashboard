a4c5707ac7a70bb42c891841be93e5a2dbd00ce1
obozhinov <olya.bozhinov@gmail.com>
2020-05-27 16:33:45 +0300

    Attempt at contract testing with spring cloud contract, dockerfile added

--- src/test/java/com/example/dashboard/DashboardApplicationTests.java
+++ src/test/java/com/example/dashboard/DashboardApplicationTests.java
@@ -1,9 +1,16 @@
 package com.example.dashboard;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.NONE)
+@AutoConfigureStubRunner(ids = {"com.example:employees:+:stubs:6565"},
+		stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 class DashboardApplicationTests {
 
 	@Test
